22903a19649050120eb21f368eb6e1ca915dfc21
Alexandra Murawski <almurawski15@gmail.com>
2021-06-18 13:34:32 -0400

    created components and connected to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import Search from "./components/Search";
+
 
 function App() {
+  document.title = "Employee Directory";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <NavBar />
+      <Wrapper>
+        <Search />
+      </Wrapper>
+      <Footer />
     </div>
   );
 }
