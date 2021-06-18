d9a176d5d694326bf6cee668be3da8274b364b95
Alexandra Murawski <almurawski15@gmail.com>
2021-06-18 13:44:45 -0400

    added initial coding to components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer>
+            <div>
+                <h1> Edit Footer Here </h1>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar () {
+    return (
+        <nav>
+            <div>
+                <h1> Search Bar Soon </h1>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
